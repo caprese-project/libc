1d36340341b307b6c7124d436c825d356a44bc3c
cosocaf <cosocaf@gmail.com>
2023-10-31 23:28:24 +0900

    Support stdalign.h

---
+++ include/stdalign.h
@@ -0,0 +1,14 @@
+#ifndef CAPRESE_LIBC_STDALIGN_H_
+#define CAPRESE_LIBC_STDALIGN_H_
+
+#ifndef __cplusplus
+
+#define alignas _Alignas
+#define alignof _Alignof
+
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
+
+#endif // !__cplusplus
+
+#endif // CAPRESE_LIBC_STDALIGN_H_
