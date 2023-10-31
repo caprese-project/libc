cb4c2612315f2df47621346e6884b0e87f282739
cosocaf <cosocaf@gmail.com>
2023-10-31 23:31:45 +0900

    Support stdnoreturn.h

---
+++ include/stdnoreturn.h
@@ -0,0 +1,8 @@
+#ifndef CAPRESE_LIBC_STDNORETURN_H_
+#define CAPRESE_LIBC_STDNORETURN_H_
+
+#ifndef __cplusplus
+#define noreturn _Noreturn
+#endif // __cplusplus
+
+#endif // CAPRESE_LIBC_STDNORETURN_H_
