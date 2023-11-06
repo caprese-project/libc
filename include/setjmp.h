91e4d21f90ca4cc39983d00da7894f971a0d5764
cosocaf <cosocaf@gmail.com>
2023-11-06 17:04:15 +0900

    setjmp

--- include/setjmp.h
+++ include/setjmp.h
@@ -4,10 +4,15 @@
 #include <internal/attribute.h>
 
 #if defined(__riscv)
-typedef long long jmp_buf;
+typedef long long __jmp_buf_elem_t;
+#define __JMP_BUF_SIZE 14 // ra, sp, s0-s11
 #else // ^^^ __riscv ^^^ / vvv !__riscv vvv
 #error "Unsupported arch"
 #endif // !__riscv
+
+typedef struct {
+  __jmp_buf_elem_t _buf[__JMP_BUF_SIZE];
+} jmp_buf[1];
 
 int             setjmp(jmp_buf env);
 __noreturn void longjmp(jmp_buf env, int val);
