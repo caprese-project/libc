fa2ff98b4441d9d98164ebddfc441be5dd20446b
cosocaf <cosocaf@gmail.com>
2023-11-06 17:10:12 +0900

    fix

--- include/setjmp.h
+++ include/setjmp.h
@@ -3,18 +3,30 @@
 
 #include <internal/attribute.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 #if defined(__riscv)
-typedef long long __jmp_buf_elem_t;
+
+  typedef long long __jmp_buf_elem_t;
 #define __JMP_BUF_SIZE 14 // ra, sp, s0-s11
+
 #else // ^^^ __riscv ^^^ / vvv !__riscv vvv
+
 #error "Unsupported arch"
+
 #endif // !__riscv
 
-typedef struct {
-  __jmp_buf_elem_t _buf[__JMP_BUF_SIZE];
-} jmp_buf[1];
+  typedef struct __jmp_buf {
+    __jmp_buf_elem_t _buf[__JMP_BUF_SIZE];
+  } jmp_buf[1];
 
-int             setjmp(jmp_buf env);
-__noreturn void longjmp(jmp_buf env, int val);
+  int             setjmp(jmp_buf env);
+  __noreturn void longjmp(jmp_buf env, int val);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
 
 #endif // CAPRESE_LIBC_SETJMP_H_
