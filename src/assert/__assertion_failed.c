55ce223c29d25832a8f7a02e6515da688fcd8951
cosocaf <cosocaf@gmail.com>
2023-11-11 01:45:09 +0900

    fix

--- src/assert/__assertion_failed.c
+++ src/assert/__assertion_failed.c
@@ -4,6 +4,6 @@
 #include <stdlib.h>
 
 __weak __noreturn void __assertion_failed(const char* file, int line, const char* function, const char* condition) {
-  fprintf(stderr, "Assertion failed: %s, %s at %s:%d", condition, function, file, line);
+  fprintf(stderr, "Assertion failed: %s, %s at %s:%d\n", condition, function, file, line);
   abort();
 }
