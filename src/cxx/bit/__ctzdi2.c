450fdfb5ed53b26eef1d3441b2ca7fcdbba3713b
cosocaf <cosocaf@gmail.com>
2023-11-19 16:44:22 +0900

    wip

---
+++ src/cxx/bit/__ctzdi2.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+int __ctzdi2(uint64_t value) {
+  if (value == 0) return 64;
+
+  int count = 0;
+
+  while ((value & 1) == 0) {
+    value >>= 1;
+    count++;
+  }
+
+  return count;
+}
