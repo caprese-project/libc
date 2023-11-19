d706e19185d8405a7d102736d1e6f9b9d784a033
cosocaf <cosocaf@gmail.com>
2023-11-19 16:32:25 +0900

    fix

--- src/cxx/bit/__popcountsi2.c
+++ src/cxx/bit/__popcountsi2.c
@@ -3,7 +3,7 @@
 int __popcountsi2(uint32_t value) {
   value = value - ((value >> 1) & 0x55555555);
   value = (value & 0x33333333) + ((value >> 2) & 0x33333333);
-  value = (x + (x >> 4)) & 0x0f0f0f0f;
+  value = (value + (value >> 4)) & 0x0f0f0f0f;
   value = value + (value >> 16);
   value = (value + (value >> 8)) & 0x3f;
   return (int)value;
