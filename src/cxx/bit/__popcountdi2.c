c9f78fd41285b9c1c9f5d7b8eaa288f5ba68833b
cosocaf <cosocaf@gmail.com>
2023-11-19 16:31:19 +0900

    fix

--- src/cxx/bit/__popcountdi2.c
+++ src/cxx/bit/__popcountdi2.c
@@ -3,7 +3,7 @@
 int __popcountdi2(uint64_t value) {
   value = value - ((value >> 1) & 0x5555555555555555ull);
   value = (value & 0x3333333333333333ull) + ((value >> 2) & 0x3333333333333333ull);
-  value = (value + (value >> 4) & 0x0f0f0f0f0f0f0f0full);
+  value = (value + (value >> 4)) & 0x0f0f0f0f0f0f0f0full;
 
   uint32_t v = (uint32_t)(value + (value >> 32));
   v          = v + (v >> 16);
