468d1db05359d9bf66af18c8a688acc4508050a6
cosocaf <cosocaf@gmail.com>
2023-12-27 20:22:50 +0900

    to_string

---
+++ src/cxx/string/to_string.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <string>
+
+namespace std {
+  string to_string(int val) {
+    char buf[32];
+    sprintf(buf, "%d", val);
+    return string(buf);
+  }
+
+  string to_string(unsigned val) {
+    char buf[32];
+    sprintf(buf, "%u", val);
+    return string(buf);
+  }
+
+  string to_string(long val) {
+    char buf[32];
+    sprintf(buf, "%ld", val);
+    return string(buf);
+  }
+
+  string to_string(unsigned long val) {
+    char buf[32];
+    sprintf(buf, "%lu", val);
+    return string(buf);
+  }
+
+  string to_string(long long val) {
+    char buf[32];
+    sprintf(buf, "%lld", val);
+    return string(buf);
+  }
+
+  string to_string(unsigned long long val) {
+    char buf[32];
+    sprintf(buf, "%llu", val);
+    return string(buf);
+  }
+} // namespace std
